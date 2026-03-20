747f53934ced4af43fda9b9110ed816425c7310e
uwu <potatocuteuwu@gmail.com>
2026-03-20 12:00:56 +0300

    xml-комментарии

--- Pr_7_Krasnova_Solodov/Program.cs
+++ Pr_7_Krasnova_Solodov/Program.cs
@@ -1,7 +1,13 @@
 ﻿using System;
 
+/// <summary>
+/// Класс программы-примера.
+/// </summary>
 class ArrayExample
 {
+    /// <summary>
+    /// Основная функция.
+    /// </summary>
     static void Main()
     {
         char[] letters = { 'f', 'r', 'e', 'd', ' ', 's', 'm', 'i', 't', 'h' };
@@ -16,6 +22,11 @@ class ArrayExample
         Console.ReadKey();
     }
 
+    /// <summary>
+    /// Выводит символы и их общее количество.
+    /// </summary>
+    /// <param name="name">Символы (имя).</param>
+    /// <param name="msg">Общее количество символов.</param>
     static void SendMessage(string name, int msg)
     {
         Console.WriteLine("Hello, " + name + "! Count to " + msg);
